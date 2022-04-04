60c1de8c761d24d15a0f50d3a36ee700a8f3d669
wood11nho <stoicaelias2003@outlook.com>
2022-04-05 00:39:16 +0300

    DFA Word Validator - 1

--- automaton.py
+++ automaton.py
@@ -2,32 +2,20 @@ class Automaton():
 
     def __init__(self, config_file):
         self.config_file = config_file
+        self.states = []
+        self.words = []
+        self.transitions = []
+        self.final = []
+        self.dictionar = {}
         print("Hi, I'm an automaton!")
 
     def validate(self):
-        """Return a Boolean
-        Returns true if the config file is valid,
-        and raises a ValidationException if the config is invalid.
-        """
-        return "I can't tell if the config file is valid... yet!"
 
-    def accepts_input(self, input_str):
-        """Return a Boolean
-        Returns True if the input is accepted,
-        and it returns False if the input is rejected.
-        """
-        pass
-
-    def read_input(self, input_str):
-
-        Alfabet = set()
-        Stari = set()
         Tranzitii = []
-        Final = set()
 
         ok = 0
 
-        input_file = open(input_str, "r")
+        input_file = open(self.config_file)
         text_input = input_file.readlines()
         for linie in text_input:
             if '#' in linie or linie == '\n':
@@ -44,47 +32,75 @@ class Automaton():
                 ok = ok + 1
                 continue
 
-            if ok == 1:         #suntem pe alfabet
-                Alfabet.add(linie.strip())
-            if ok == 2:         #suntem pe stari
-                #cazul 1, stari care pot fi finale SI initiale, in acelasi timp
+            if ok == 1:  # suntem pe alfabet
+                self.words.append(linie.strip())
+            if ok == 2:  # suntem pe stari
+                # cazul 1, stari care pot fi finale SI initiale, in acelasi timp
                 if len(linie.strip().split(',')) > 2:
-                    Stari.add(int(linie.strip().split()[0]))
+                    self.states.append(int(linie.strip().split()[0]))
                     if linie.strip().split(',')[2] == 'F' or linie.strip().split(',')[1] == 'F':
-                        Final.add(linie.strip().split(',')[0])
+                        self.final.append(int(linie.strip().split(',')[0]))
                     if linie.strip().split(',')[2] == 'S' or linie.strip().split(',')[1] == 'S':
-                        Initial = linie.strip().split(',')[0]
-                #cazul 2, stari care sunt doar finale SAU doar initiale
+                        self.initial = int(linie.strip().split(',')[0])
+                # cazul 2, stari care sunt doar finale SAU doar initiale
                 elif len(linie.strip().split(',')) == 2:
-                    Stari.add(int(linie.strip().split()[0]))
+                    self.states.append(int(linie.strip().split()[0]))
                     if linie.strip().split(',')[1] == 'S':
-                        Initial = linie.strip().split(',')[0]
+                        self.initial = int(linie.strip().split(',')[0])
                     if linie.strip().split(',')[1] == 'F':
-                        Final.add(linie.strip().split(',')[0])
-                #cazul 3, stari care nu sunt nici finale, nici initiale
+                        self.final.append(int(linie.strip().split(',')[0]))
+                # cazul 3, stari care nu sunt nici finale, nici initiale
                 else:
-                    Stari.add(int(linie.strip()))
-            if ok == 3:         #suntem pe tranzitii
+                    self.states.append(int(linie.strip()))
+            if ok == 3:  # suntem pe tranzitii
 
                 tranzitie = linie.strip().split(", ")
                 tranzitie[0] = int(tranzitie[0])
                 tranzitie[2] = int(tranzitie[2])
                 tranzitie[1] = tranzitie[1][0:1]
-                Tranzitii.append(tuple(tranzitie))
+                self.transitions.append(tuple(tranzitie))
 
         verificare = 1
-        print(Stari)
-        print(Tranzitii)
-        for tranzitie in Tranzitii:
-            if tranzitie[0] not in Stari or tranzitie[1] not in Alfabet or tranzitie[2] not in Stari:
+        # print(Stari)
+        # print(Initial)
+        # print(Tranzitii)
+        # print(Final)
+        self.words.remove("Sigma :")
+        # print(Alfabet)
+        Tranzitii_stari = {}
+        # fac un dictionar cu tranzitiile, pt fiecare stare
+        for tranzitie in self.transitions:
+            if tranzitie[0] not in Tranzitii_stari:
+                Tranzitii_stari[tranzitie[0]] = dict()
+                Tranzitii_stari[tranzitie[0]][tranzitie[1]] = tranzitie[2]
+            else:
+                Tranzitii_stari[tranzitie[0]][tranzitie[1]] = tranzitie[2]
+        # print(Tranzitii_stari)
+        self.dictionar = Tranzitii_stari
+        # print(self.states)
+        # print(self.final)
+        # print(self.initial)
+        # print(self.dictionar)
+        for tranzitie in self.transitions:
+            if tranzitie[0] not in self.states or tranzitie[1] not in self.words or tranzitie[2] not in self.states:
                 print("INPUT NEVALID!!! NASPA")
-                verificare = 0
-                return 0
         if verificare == 1:
             print("INPUT VALID!!! BRAVO")
-        return 1
+            print("Este valid inputul?")
+            return True
+
+    def accepts_input(self, input_str):
+        """Return a Boolean
+        Returns True if the input is accepted,
+        and it returns False if the input is rejected.
+        """
+        pass
+
+    def read_input(self, input_str):
+        pass
+
 
 
 if __name__ == "__main__":
     a = Automaton('date.in')
-    a.read_input('date.in')
+    print(a.validate())
