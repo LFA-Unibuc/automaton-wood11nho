14a01e7ea836cd19a4a379d3c4ef02647e84c503
wood11nho <stoicaelias2003@outlook.com>
2022-05-01 13:17:21 +0300

    update pentru NFA

--- automaton.py
+++ automaton.py
@@ -37,30 +37,31 @@ class Automaton():
             if ok == 2:  # suntem pe stari
                 # cazul 1, stari care pot fi finale SI initiale, in acelasi timp
                 if len(linie.strip().split(',')) > 2:
-                    self.states.append(int(linie.strip().split()[0]))
+                    self.states.append(linie.strip().split()[0])
                     if linie.strip().split(',')[2] == 'F' or linie.strip().split(',')[1] == 'F':
-                        self.final.append(int(linie.strip().split(',')[0]))
+                        self.final.append(linie.strip().split(' ,')[0])
                     if linie.strip().split(',')[2] == 'S' or linie.strip().split(',')[1] == 'S':
-                        self.initial = int(linie.strip().split(',')[0])
+                        self.initial = linie.strip().split(' ,')[0]
                 # cazul 2, stari care sunt doar finale SAU doar initiale
                 elif len(linie.strip().split(',')) == 2:
-                    self.states.append(int(linie.strip().split()[0]))
+                    self.states.append(linie.strip().split()[0])
                     if linie.strip().split(',')[1] == 'S':
-                        self.initial = int(linie.strip().split(',')[0])
+                        self.initial = linie.strip().split(' ,')[0]
                     if linie.strip().split(',')[1] == 'F':
-                        self.final.append(int(linie.strip().split(',')[0]))
+                        self.final.append(linie.strip().split(' ,')[0])
                 # cazul 3, stari care nu sunt nici finale, nici initiale
                 else:
-                    self.states.append(int(linie.strip()))
+                    self.states.append(linie.strip())
             if ok == 3:  # suntem pe tranzitii
 
                 tranzitie = linie.strip().split(", ")
-                tranzitie[0] = int(tranzitie[0])
-                tranzitie[2] = int(tranzitie[2])
+                tranzitie[0] = tranzitie[0]
+                tranzitie[2] = tranzitie[2]
                 tranzitie[1] = tranzitie[1][0:1]
                 self.transitions.append(tuple(tranzitie))
 
         verificare = 1
+        # print(self.transitions)
         # print(Stari)
         # print(Initial)
         # print(Tranzitii)
@@ -72,21 +73,34 @@ class Automaton():
         for tranzitie in self.transitions:
             if tranzitie[0] not in Tranzitii_stari:
                 Tranzitii_stari[tranzitie[0]] = dict()
-                Tranzitii_stari[tranzitie[0]][tranzitie[1]] = tranzitie[2]
+                Tranzitii_stari[tranzitie[0]][tranzitie[1]] = [tranzitie[2]]
             else:
-                Tranzitii_stari[tranzitie[0]][tranzitie[1]] = tranzitie[2]
+                if tranzitie[1] in Tranzitii_stari[tranzitie[0]]:
+                    Tranzitii_stari[tranzitie[0]][tranzitie[1]].append(tranzitie[2])
+                else:
+                    Tranzitii_stari[tranzitie[0]][tranzitie[1]] = [tranzitie[2]]
+        for stare in self.states:
+            if stare not in Tranzitii_stari:
+                Tranzitii_stari[stare] = {}
+                for cuvant in self.words:
+                    Tranzitii_stari[stare][cuvant] = []
+        for elem in Tranzitii_stari:
+            for cuvant in self.words:
+                if cuvant not in Tranzitii_stari[elem]:
+                    Tranzitii_stari[elem][cuvant] = []
         # print(Tranzitii_stari)
         self.dictionar = Tranzitii_stari
+        # print(self.words)
         # print(self.states)
         # print(self.final)
         # print(self.initial)
         # print(self.dictionar)
+        
         for tranzitie in self.transitions:
             if tranzitie[0] not in self.states or tranzitie[1] not in self.words or tranzitie[2] not in self.states:
                 print("INPUT NEVALID!!! NASPA")
         if verificare == 1:
-            print("INPUT VALID!!! BRAVO")
-            print("Este valid inputul?")
+            print("In primul rand, este valid inputul?")
             return True
 
     def accepts_input(self, input_str):
